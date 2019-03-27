9f749dd0857c857bdeed1aa8d55da21d4c22858b
luben <lubenfrank48@gmail.com>
2019-03-27 09:52:02 +0530

    pattern without repeat

--- assignments/Coding Challenges/pattern/app.js
+++ assignments/Coding Challenges/pattern/app.js
@@ -1,12 +1,32 @@
 "use strict";
 
+// using repeat
+// var pattern = n => {
+//   var pattern = "*";
+//   for (var i = 1; i <= n; i++) {
+//     console.log(pattern.repeat(i));
+//   }
+//   for (var j = n; j >= 1; j--) {
+//     console.log(pattern.repeat(j));
+//   }
+// };
+
+// without using repeat
 var pattern = n => {
-  var pattern = "*";
+  var pattern = "";
   for (var i = 1; i <= n; i++) {
-    console.log(pattern.repeat(i));
+    pattern += "*";
+    console.log(pattern);
   }
+
   for (var j = n; j >= 1; j--) {
-    console.log(pattern.repeat(j));
+    var number = j;
+    pattern = "";
+    while (number >= 1) {
+      pattern += "*";
+      number--;
+    }
+    console.log(pattern);
   }
 };
 
