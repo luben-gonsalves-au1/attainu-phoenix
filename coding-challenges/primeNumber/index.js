fb31eb184a7db86cd59b2838e9aebca49f59da99
luben <lubenfrank48@gmail.com>
2019-05-20 11:15:12 +0530

    prime number

--- coding-challenges/primeNumber/index.js
+++ coding-challenges/primeNumber/index.js
@@ -7,4 +7,25 @@ var prime = (num) => {
     return console.log("its a prime number")
 }
 
-prime(1);
+prime(5);
+
+
+var primeArray = (num) => {
+    var arr = [];
+    while (num > 1) {
+        for (var i = 2; i < num; i++) {
+            flag = 0;
+            if (num % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) {
+            arr.push(num)
+        }
+        num--;
+    }
+    return arr.reverse();
+}
+
+console.log(primeArray(35));
