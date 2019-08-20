cb09dbb4411740c4a8ae11eebf154ae10a3489a2
luben <lubenfrank48@gmail.com>
2019-08-20 10:06:40 +0530

    update

--- coding-challenges/week 27/day 2/index.js
+++ coding-challenges/week 27/day 2/index.js
@@ -1,18 +1,17 @@
-function permutation (array, k = array.length) {
-  if(k == 1) {
+function permutation(array, k = array.length) {
+  if (k == 1) {
     console.log(array);
   } else {
     permutation(array, k - 1);
-    for(var i = 0; i < k - 1; i += 1) {
-      if(k %2 == 0) {
-    [array[i], array[k -1]] = [array[k - 1], array[i]]
-       } else {
-       [array[0], array[k -1]] = [array[0], array[k - 1]]
+    for (var i = 0; i < k - 1; i += 1) {
+      if (k % 2 == 0) {
+        [array[i], array[k - 1]] = [array[k - 1], array[i]];
+      } else {
+        [array[0], array[k - 1]] = [array[0], array[k - 1]];
+      }
+      permutation(array, k - 1);
     }
-    permutation(array, k - 1);
   }
 }
-}
 
-
-permutation(["A","B","C"])
+permutation(["A", "B", "C"]);
