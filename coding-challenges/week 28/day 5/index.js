94c5a978261e06ca4f94a6fb4acf707c3b140f38
luben <lubenfrank48@gmail.com>
2019-08-30 10:22:11 +0530

    pairSum

--- coding-challenges/week 28/day 5/index.js
+++ coding-challenges/week 28/day 5/index.js
@@ -1,14 +1,15 @@
 function pairSum(array, target) {
-  var obj = {};
+  let obj = {};
+  let result = [];
   for (let num of array) {
     let potentialSum = target - num;
     if (potentialSum in obj) {
-      return [num, potentialSum];
+      result.push([num, potentialSum]);
     } else {
       obj[num] = true;
     }
   }
-  return [];
+  return result;
 }
 
 console.log(pairSum([1, 2, 4, 5, 6, 10, 12], 10));
