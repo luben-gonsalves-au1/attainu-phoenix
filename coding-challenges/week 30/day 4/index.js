516a23b525c6f34b90955f64deb95c72bcbffe56
luben <lubenfrank48@gmail.com>
2019-09-12 21:48:12 +0530

    parenthesis updated need

--- coding-challenges/week 30/day 4/index.js
+++ coding-challenges/week 30/day 4/index.js
@@ -1,33 +1,54 @@
-function parenthesis(string) {
-  var stack = [];
 
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === "(" || string[i] === "[" || string[i] === "{") {
-      stack.push(string[i]);
-    } else if (
-      string[i] === ")" &&
-      stack.length > 0 &&
-      stack[stack.length - 1] == "("
-    ) {
-      stack.pop();
-    } else if (
-      string[i] === "]" &&
-      stack.length > 0 &&
-      stack[stack.length - 1] == "["
-    ) {
-      stack.pop();
-    } else if (
-      string[i] === "}" &&
-      stack.length > 0 &&
-      stack[stack.length - 1] == "{"
-    ) {
-      stack.pop();
-    } else {
-      return false;
-    }
-  }
 
-  return stack.length === 0;
-}
 
-console.log(parenthesis(")"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function parenthesis(string) {
+//   var stack = [];
+
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "(" || string[i] === "[" || string[i] === "{") {
+//       stack.push(string[i]);
+//     } else if (
+//       string[i] === ")" &&
+//       stack.length > 0 &&
+//       stack[stack.length - 1] == "("
+//     ) {
+//       stack.pop();
+//     } else if (
+//       string[i] === "]" &&
+//       stack.length > 0 &&
+//       stack[stack.length - 1] == "["
+//     ) {
+//       stack.pop();
+//     } else if (
+//       string[i] === "}" &&
+//       stack.length > 0 &&
+//       stack[stack.length - 1] == "{"
+//     ) {
+//       stack.pop();
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   return stack.length === 0;
+// }
+
+// console.log(parenthesis(")"));
